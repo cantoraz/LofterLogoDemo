2e23bc1718fa5ae99d1f053551afbec6849879fa
Cantoraz Chou <cantoraz@gmail.com>
2016-07-19 14:12:34 +0800

    Initial Commit

---
+++ LofterLogoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LofterLogoDemo
+//
+//  Created by Cantoraz Chou on 7/19/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LofterLogoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LofterLogoDemo
+//
+//  Created by Cantoraz Chou on 7/19/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
